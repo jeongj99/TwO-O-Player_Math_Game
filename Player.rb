0183623563c1cbee38c4f2be560125dd538c4f96
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-14 18:43:47 +0000

    Add lose_life and dead? method to class Player

--- Player.rb
+++ Player.rb
@@ -1,8 +1,17 @@
 class Player
-  attr_reader :name, :lives
+  attr_reader :name
+  attr_accessor :lives
 
   def initialize(name)
     @name = name
     @lives = 3
+  end
+
+  def lose_life
+    self.lives -= 1 if lives > 0
+  end
+
+  def dead?
+    self.lives == 0
   end
 end
