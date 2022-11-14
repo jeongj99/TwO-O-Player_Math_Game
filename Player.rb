3ccf806215e2976892b390ed2efb2c5655763d4d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-14 22:54:00 +0000

    Add question_for_player method

--- Player.rb
+++ Player.rb
@@ -7,6 +7,14 @@ class Player
     @lives = 3
   end
 
+  def question_for_player
+    question = Question.new
+    puts "#{self.name}: #{question.ask}"
+    print '> '
+    player_answer = $stdin.gets.chomp
+    question.check_answer(player_answer.to_i)
+  end
+
   def lose_life
     self.lives -= 1 if lives > 0
   end
